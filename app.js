a0db0b1b00c05ecc1b49b762a42fca98c5735e94
chadrackmbala <chadrackmbala20@gmail.com>
2023-07-29 20:57:09 +0100

    use innerHTML

--- app.js
+++ app.js
@@ -5,5 +5,19 @@
 const parent = document.getElementById('parent');
 const enfant = document.getElementById('enfant');
 
-parent.removeChild(enfant);
-console.log(enfant.parentNode);
+// console.log(enfant.parentNode);
+// parent.removeChild(enfant);
+// console.log(enfant.parentNode);
+
+// const nouveauBouton = document.createElement("button");
+
+// var p = document.createElement("p");
+// document.body.appendChild(p);
+
+const div = document.querySelector('h1');
+
+// console.log(div);
+
+console.log(div.innerHTML);
+div.innerHTML = 'Hello';
+console.log(div.innerHTML);
