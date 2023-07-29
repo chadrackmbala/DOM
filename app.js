2ae3fd02f52ef5b6c24f3e3b89960cd58df771b7
chadrackmbala <chadrackmbala20@gmail.com>
2023-07-29 19:58:10 +0100

    first send

---
+++ app.js
@@ -0,0 +1,9 @@
+// for(i = 0; i <= 1000; i++) {
+//     console.log(i);
+// }
+
+const parent = document.getElementById('parent');
+const enfant = document.getElementById('enfant');
+
+// parent.removeChild(enfant);
+console.log(enfant.parentNode);
