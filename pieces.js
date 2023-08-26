a9787d9cdec9b898472c616f598c9f19885974bc
Chadrack Mbala <102918698+chadrackmbala@users.noreply.github.com>
2023-08-26 16:42:25 -0700

    Add files via upload

--- pieces.js
+++ pieces.js
@@ -2,26 +2,36 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-const article = pieces[1];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "aucune description pour le moment";
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "aucune catégorie disponible";
-const stockElement = document.createElement("p");
-stockElement.innerText = article.disponibilite ? "En stock" : "Stock epuisé !"
+for (let i = 0; i < pieces.length; i++) {
 
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockElement);
-
-article.prix < 35 ? "€" : "€€€"
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // On crée l’élément img.
+    const imageElement = document.createElement("img");
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = pieces[i].nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${pieces[i].prix} € (${pieces[i].prix < 35 ? "€" : "€€€"})`;
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = pieces[i].description ?? "aucune description pour le moment";
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = pieces[i].categorie ?? "aucune catégorie disponible";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = pieces[i].disponibilite ? "En stock" : "Stock epuisé !";
+    // On accède à l’indice i de la liste pieces pour configurer la source de l’image.
+    imageElement.src = pieces[i].image;
+    // Idem pour le nom, le prix et la catégorie...
+    
+    // On rattache la balise article à la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
+    // Idem pour le nom, le prix et la catégorie...
+    }
